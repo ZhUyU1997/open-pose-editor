11fe543976a38c314dd5d782b4943a84923e241d
Yu Zhu <891085309@qq.com>
2023-03-10 03:02:45 +0800

    🐞 fix: too long link

--- src/config.ts
+++ src/config.ts
@@ -4,8 +4,8 @@ export const options: Record<string, any> = {
     clearColor: 0xaaaaaa,
     moveMode: false,
     changeLanguage() {
-        let url = new URL(window.location.href)
-        url.searchParams.append('lng', i18n.language === 'zh' ? 'en' : 'zh')
+        const url = new URL(window.location.href)
+        url.searchParams.set('lng', i18n.language === 'zh' ? 'en' : 'zh')
         window.location.assign(url)
     },
 }
