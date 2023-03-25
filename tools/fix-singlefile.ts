7830ed8fe09d0088a1c671b16f484a14603757f2
Yu Zhu <891085309@qq.com>
2023-03-26 02:14:11 +0800

    🐎 ci: release

--- tools/fix-singlefile.ts
+++ tools/fix-singlefile.ts
@@ -1,8 +1,16 @@
 import fs from 'fs-extra'
+import { zip } from 'zip-a-folder'
+import path from 'path'
 
-const content = fs.readFileSync('dist/index.html').toString()
+async function main() {
+    const content = fs.readFileSync(path.join('dist', 'index.html')).toString()
 
-fs.writeFile(
-    'dist/index.html',
-    content.replace(`type="module" crossorigin`, 'defer')
-)
+    fs.writeFileSync(
+        path.join('dist', 'index.html'),
+        content.replace(`type="module" crossorigin`, 'defer')
+    )
+
+    await zip('dist', path.join('html.zip'))
+}
+
+main()
