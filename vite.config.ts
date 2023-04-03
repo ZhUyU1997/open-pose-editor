17e0552282a2aa54395f263be63bb0504d7fa1c1
Yu Zhu <891085309@qq.com>
2023-04-03 22:30:42 +0800

    ✨ feat: pwa requirement

--- vite.config.ts
+++ vite.config.ts
@@ -28,13 +28,65 @@ const config: UserConfigFn = ({ command, mode, ssrBuild }) => {
                 },
             ],
         },
+        includeAssets: [
+            'favicon.ico',
+            'apple-touch-icon.png',
+            'safari-pinned-tab.svg',
+        ],
         manifest: {
-            name: 'open pose editor',
-            short_name: 'open pose editor',
-            description: 'open pose editor (Yu Zhu)',
+            name: '3D Openpose Editor',
+            short_name: '3D Openpose Editor',
+            description: '3D Openpose Editor (Yu Zhu)',
             theme_color: '#ffffff',
             background_color: '#ffffff',
-            display: 'standalone',
+            icons: [
+                {
+                    src: 'icons/icon-72x72.png',
+                    sizes: '72x72',
+                    type: 'image/png',
+                },
+                {
+                    src: 'icons/icon-96x96.png',
+                    sizes: '96x96',
+                    type: 'image/png',
+                },
+                {
+                    src: 'icons/icon-128x128.png',
+                    sizes: '128x128',
+                    type: 'image/png',
+                },
+                {
+                    src: 'icons/icon-144x144.png',
+                    sizes: '144x144',
+                    type: 'image/png',
+                },
+                {
+                    src: 'icons/icon-152x152.png',
+                    sizes: '152x152',
+                    type: 'image/png',
+                },
+                {
+                    src: 'icons/icon-192x192.png',
+                    sizes: '192x192',
+                    type: 'image/png',
+                },
+                {
+                    src: 'icons/icon-384x384.png',
+                    sizes: '384x384',
+                    type: 'image/png',
+                },
+                {
+                    src: 'icons/icon-512x512.png',
+                    sizes: '512x512',
+                    type: 'image/png',
+                },
+                {
+                    src: 'icons/icon-512x512.png',
+                    sizes: '512x512',
+                    type: 'image/png',
+                    purpose: 'maskable any',
+                },
+            ],
         },
     })
 
